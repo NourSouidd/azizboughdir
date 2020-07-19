8fea3addcbb098411c9df19be2c1eb91a6b1629c
Nour Souid <noursouidd@gmail.com>
2020-07-19 00:14:33 -0400

    add header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react"
 
+import '../styles/styles.scss'
+
+import Header from '../components/header';
+
 const IndexPage = () => (
-  <p>Hello World</p>
+  <div>
+    <Header />
+  </div>
 )
 
 export default IndexPage
