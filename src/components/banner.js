611cccb1360e69718bd39e790229d50190deeb5f
Nour Souid <noursouidd@gmail.com>
2020-07-20 21:02:14 -0400

    placed images in the banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,13 +1,48 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import { distribute } from "gsap/gsap-core"
 
 const Banner = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            middlePic: file(relativePath: { eq: "portfolio3.jpg" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            leftPic: file(relativePath: { eq: "portfolio2.jpg" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            rightPic: file(relativePath: { eq: "portfolio4.jpg" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `)
     return (
         <div className="banner">
             <div className="container">
                 <div className="row">
-                    <div className="main-text">Graphic Designer</div>
+                    <div className="main-text">Aziz Boughdir</div>
+                    <div className="side-img left">
+                        <Img fluid={data.leftPic.childImageSharp.fluid} />
+                    </div>
+                    <div className="main-img">
+                        <Img fluid={data.middlePic.childImageSharp.fluid} />
+                    </div>
+                    <div className="side-img right">
+                        <Img fluid={data.rightPic.childImageSharp.fluid} />
+                    </div>
                 </div>
             </div>
         </div>
