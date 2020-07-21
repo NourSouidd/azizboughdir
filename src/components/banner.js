90a90348898bf67e6f0101e29386394b0525519e
Nour Souid <noursouidd@gmail.com>
2020-07-21 17:45:35 -0400

    removed pictures, made a video background

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,40 +1,52 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
-import { distribute } from "gsap/gsap-core"
+import Boom from "../videos/boom.mp4"
+// import { useStaticQuery, graphql } from "gatsby"
+// import Img from "gatsby-image"
+// import { distribute } from "gsap/gsap-core"
 
 const Banner = () => {
-    const data = useStaticQuery(graphql`
-        query {
-            middlePic: file(relativePath: { eq: "portfolio3.jpg" }) {
-                childImageSharp {
-                    fluid(maxWidth: 300) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-            leftPic: file(relativePath: { eq: "portfolio2.jpg" }) {
-                childImageSharp {
-                    fluid(maxWidth: 300) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-            rightPic: file(relativePath: { eq: "portfolio4.jpg" }) {
-                childImageSharp {
-                    fluid(maxWidth: 300) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-        }
-    `)
+    // const data = useStaticQuery(graphql`
+    //     query {
+    //         middlePic: file(relativePath: { eq: "portfolio3.jpg" }) {
+    //             childImageSharp {
+    //                 fluid(maxWidth: 300) {
+    //                     ...GatsbyImageSharpFluid
+    //                 }
+    //             }
+    //         }
+    //         leftPic: file(relativePath: { eq: "portfolio2.jpg" }) {
+    //             childImageSharp {
+    //                 fluid(maxWidth: 300) {
+    //                     ...GatsbyImageSharpFluid
+    //                 }
+    //             }
+    //         }
+    //         rightPic: file(relativePath: { eq: "portfolio4.jpg" }) {
+    //             childImageSharp {
+    //                 fluid(maxWidth: 300) {
+    //                     ...GatsbyImageSharpFluid
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `)
     return (
         <div className="banner">
+            <video
+              id="myVideo"
+              loop
+              muted
+              autoPlay
+            >
+              <source 
+               src={Boom}
+               type="video/mp4"
+              />
+            </video>
             <div className="container">
                 <div className="row">
                     <div className="main-text">Aziz Boughdir</div>
-                    <div className="side-img left">
+                    {/* <div className="side-img left">
                         <Img fluid={data.leftPic.childImageSharp.fluid} />
                     </div>
                     <div className="main-img">
@@ -42,11 +54,12 @@ const Banner = () => {
                     </div>
                     <div className="side-img right">
                         <Img fluid={data.rightPic.childImageSharp.fluid} />
-                    </div>
+                    </div> */}
                 </div>
                 <div className="scroll">
                     <span>Scroll Down</span>
                 </div>
+                <div className="fixed-title">Graphic Designer<i class="fas fa-circle"></i>Photographer</div>
             </div>
         </div>
         
