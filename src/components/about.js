174d2821e722bd8e2446693b60d0316cf6b11713
Nour Souid <noursouidd@gmail.com>
2020-07-22 01:08:39 -0400

    fixed banner and made navbar sticky

---
+++ src/components/about.js
@@ -0,0 +1,78 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const About = () => (
+<div className="about">
+    <div className="container">
+        <div className="inner-about">
+            <div className="content">
+              <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, 
+              making it over 2000 years old. Richard McClintock, a Latin professor at 
+              Hampden-Sydney College in Virginia, looked up one of the more obscure 
+              Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+              the cites of the word in classical literature, discovered the undoubtable 
+              source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus 
+            </p>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, 
+              making it over 2000 years old. Richard McClintock, a Latin professor at 
+              Hampden-Sydney College in Virginia, looked up one of the more obscure 
+              Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+              the cites of the word in classical literature, discovered the undoubtable 
+              source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus 
+            </p>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, 
+              making it over 2000 years old. Richard McClintock, a Latin professor at 
+              Hampden-Sydney College in Virginia, looked up one of the more obscure 
+              Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+              the cites of the word in classical literature, discovered the undoubtable 
+              source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus 
+            </p>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, 
+              making it over 2000 years old. Richard McClintock, a Latin professor at 
+              Hampden-Sydney College in Virginia, looked up one of the more obscure 
+              Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+              the cites of the word in classical literature, discovered the undoubtable 
+              source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus 
+            </p>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, 
+              making it over 2000 years old. Richard McClintock, a Latin professor at 
+              Hampden-Sydney College in Virginia, looked up one of the more obscure 
+              Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+              the cites of the word in classical literature, discovered the undoubtable 
+              source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus 
+            </p>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, 
+              making it over 2000 years old. Richard McClintock, a Latin professor at 
+              Hampden-Sydney College in Virginia, looked up one of the more obscure 
+              Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+              the cites of the word in classical literature, discovered the undoubtable 
+              source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus 
+            </p>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, 
+              making it over 2000 years old. Richard McClintock, a Latin professor at 
+              Hampden-Sydney College in Virginia, looked up one of the more obscure 
+              Latin words, consectetur, from a Lorem Ipsum passage, and going through 
+              the cites of the word in classical literature, discovered the undoubtable 
+              source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus 
+            </p>
+            </div>
+        </div>
+    </div>
+</div> 
+)
+
+export default About
--- src/components/banner.js
+++ src/components/banner.js
@@ -59,7 +59,7 @@ const Banner = () => {
                 <div className="scroll">
                     <span>Scroll Down</span>
                 </div>
-                <div className="fixed-title">Visual Artist</div>
+                <div className="fixed-title">Contempory Artist</div>
             </div>
         </div>
         
