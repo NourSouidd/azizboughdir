8f557111930ae9621ee8e6d728612768a0e8dba3
Nour Souid <noursouidd@gmail.com>
2020-07-31 19:23:26 -0400

    add projects with name and picture

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,36 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Boom from "../videos/boom.mp4"
-// import { useStaticQuery, graphql } from "gatsby"
-// import Img from "gatsby-image"
-// import { distribute } from "gsap/gsap-core"
 
 const Banner = () => {
-    // const data = useStaticQuery(graphql`
-    //     query {
-    //         middlePic: file(relativePath: { eq: "portfolio3.jpg" }) {
-    //             childImageSharp {
-    //                 fluid(maxWidth: 300) {
-    //                     ...GatsbyImageSharpFluid
-    //                 }
-    //             }
-    //         }
-    //         leftPic: file(relativePath: { eq: "portfolio2.jpg" }) {
-    //             childImageSharp {
-    //                 fluid(maxWidth: 300) {
-    //                     ...GatsbyImageSharpFluid
-    //                 }
-    //             }
-    //         }
-    //         rightPic: file(relativePath: { eq: "portfolio4.jpg" }) {
-    //             childImageSharp {
-    //                 fluid(maxWidth: 300) {
-    //                     ...GatsbyImageSharpFluid
-    //                 }
-    //             }
-    //         }
-    //     }
-    // `)
     return (
         <div className="banner">
             <video
@@ -48,23 +20,13 @@ const Banner = () => {
             <div className="container">
                 <div className="row">
                     <div className="main-text">AZIZ BOUGHDIR</div>
-                    {/* <div className="side-img left">
-                        <Img fluid={data.leftPic.childImageSharp.fluid} />
-                    </div>
-                    <div className="main-img">
-                        <Img fluid={data.middlePic.childImageSharp.fluid} />
-                    </div>
-                    <div className="side-img right">
-                        <Img fluid={data.rightPic.childImageSharp.fluid} />
-                    </div> */}
                 </div>
                 <div className="scroll">
                   <Link to="/work-page">Discover Work  <FontAwesomeIcon className="icon" icon="angle-right" /></Link>
                 </div>
                 <div className="fixed-text">Contemporary Artist</div>
             </div>
-        </div>
-        
+        </div>   
     )
 }
 export default Banner
--- src/components/work.js
+++ src/components/work.js
@@ -1,29 +1,54 @@
-import { Link } from "gatsby"
 import React from "react"
-// import { useStaticQuery, graphql } from "gatsby"
-// import Img from "gatsby-image"
-// import { distribute } from "gsap/gsap-core"
+import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { distribute } from "gsap/gsap-core"
 
 const Work = () => {
-
+    const data = useStaticQuery(graphql`
+        query {
+            firstWork: file(relativePath: { eq: "boom.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            secondWork: file(relativePath: { eq: "marche.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            thirdWork: file(relativePath: { eq: "seminaire.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `)
   return(
     <div className="work">
         <div className="container">
             <div className="inner-work">
                 <div className="content">
-                    <h3>The ability to create</h3>
-                    <p>
-                      Lorem Ipsum is simply dummy text of the printing 
-                      and typesetting industry. Lorem Ipsum has been the 
-                      industry's standard dummy text ever since the 1500s, when an 
-                      unknown printer took a galley of type and scrambled it to make a 
-                      type specimen book. It has survived not only five centuries, 
-                      but also the leap into electronic typesetting, 
-                      remaining essentially unchanged. It was popularised in the 
-                      1960s with the release of Letraset sheets containing Lorem Ipsum 
-                      passages, and more recently with desktop publishing software like 
-                      Aldus PageMaker including versions of Lorem Ipsum.
-                    </p>
+                   <div className="projects">
+                       <Link to="/" className="project">
+                          <Img fluid={data.firstWork.childImageSharp.fluid} />
+                          <h1>Foom</h1>
+                       </Link>
+                       <Link to="/" className="project">
+                          <Img fluid={data.secondWork.childImageSharp.fluid} />
+                          <h1>Imchi</h1>
+                       </Link>
+                       <Link to="/" className="project">
+                          <Img fluid={data.thirdWork.childImageSharp.fluid} />
+                          <h1>Naissance Oculaire</h1>
+                       </Link>
+                    </div>
                 </div>
             </div>
         </div>
