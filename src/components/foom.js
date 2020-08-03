dde5ec0d644235cc9fd2615212cf66603fdbfc80
Nour Souid <noursouidd@gmail.com>
2020-08-02 22:08:18 -0400

    styled foom page

--- src/components/foom.js
+++ src/components/foom.js
@@ -1,9 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
 import Boom from "../videos/boom.mp4"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { distribute } from "gsap/gsap-core"
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Foom = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        firstpro: file(relativePath: { eq: "foom1.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 300) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        secondpro: file(relativePath: { eq: "foom2.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 300) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        thirdpro: file(relativePath: { eq: "foom3.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 300) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`)
     return (
         <div className="prj">
             <div className="container">
@@ -11,7 +39,7 @@ const Foom = () => {
                   <video
                     muted
                     autoPlay
-                    controls
+                    loop
                   >
                     <source 
                       src={Boom}
@@ -19,7 +47,32 @@ const Foom = () => {
                     />
                   </video>
                   <div className="prj-imgs">
-
+                    <Img fluid={data.firstpro.childImageSharp.fluid} />
+                    <Img fluid={data.secondpro.childImageSharp.fluid} />
+                    <Img fluid={data.thirdpro.childImageSharp.fluid} />
+                  </div>
+                  <div className="prj-more">
+                    <div className="prj-desc">
+                        <div className="prj-title">
+                         <h1>Foom</h1>
+                         <span>2020</span>
+                        </div>
+                        <p>Animation Video</p>
+                    </div>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing 
+                        and typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and 
+                        scrambled it to make a type specimen book. 
+                        It has survived not only five centuries, but 
+                        also the leap into electronic typesetting, 
+                        remaining essentially unchanged. It was popularised 
+                        in the 1960s with the release of Letraset sheets 
+                        containing Lorem Ipsum passages, and more recently 
+                        with desktop publishing software like Aldus PageMaker 
+                        including versions of Lorem Ipsum.
+                    </p>
                   </div>
                 </div>
 
