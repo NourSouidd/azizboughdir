4a4f7ac825e1ec60fc2740d76366e528eb6c36ef
Nour Souid <noursouidd@gmail.com>
2020-08-08 16:48:01 -0400

    styled components plugin and refactored header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,82 @@
 import { Link } from "gatsby"
 import React from "react"
+import styled from 'styled-components'
+
+const StyledHeader = styled.Header`
+  height: 80px;
+  width: 100%;
+  position: fixed;
+  background-color: $black;
+  top: 0px;
+  z-index: 10;
+`
+
+const StyledContainer = styled.div`
+  width: 1640px;
+  min-width: 1640px;
+  margin: 0 auto;
+  @media (max-width: 1440px) {
+      width: 1280px;
+      min-width: 1280px;
+  }
+  @media (max-width: 1280px) {
+      width: 976px;
+      min-width: 976px;
+  }
+  @media (max-width: 976px) {
+      width: 768px;
+      min-width: 768px;
+  }
+`
+
+const StyledInnerHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 80px;
+`
+
+const StyledLogo = styled.div`
+  & a {
+    font-weight: 700;
+    text-decoration: none;
+    letter-spacing: 1.5px;
+    color: $white;
+  }
+`
+const StyledNavigation = styled.div`
+  & nav {
+    & a {
+      margin: 0 20px;
+      text-decoration: none;
+      letter-spacing: 1.75px;
+      text-transform: uppercase;
+      font-size: 0.75rem;
+      color: $white;
+      &:last-child {
+        margin-right: 0px;
+      }
+    }
+  }
+`
 
 const Header = () => (
-  <header>
-    <div className="container">
-      <div className="inner-header">
-        <div className="logo">
-            <Link to="/">BOUGHDIR</Link>
-          </div>
-          <div className="navigation">
-            <nav>
-              <Link to="/about-page">About</Link>
-              <Link to="/work-page">Work</Link>
-              <Link to="/contact">Contact</Link>
-            </nav>
-        </div>
-      </div>
-    </div>
-  </header>
+  <StyledHeader>
+    <StyledContainer>
+      <StyledInnerHeader>
+        <StyledLogo>
+          <Link to="/">BOUGHDIR</Link>
+        </StyledLogo>
+        <StyledNavigation>
+          <nav>
+            <Link to="/about-page">About</Link>
+            <Link to="/work-page">Work</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
+        </StyledNavigation>
+      </StyledInnerHeader>
+    </StyledContainer>
+  </StyledHeader>
 )
 
 export default Header
