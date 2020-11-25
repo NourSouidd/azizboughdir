d2f4351ba6db787b0028d67625373d3ac8182008
Nour Souid <noursouidd@gmail.com>
2020-11-25 22:17:48 +0100

    added style sheet to seminaire page

--- src/pages/seminaire-page.js
+++ src/pages/seminaire-page.js
@@ -1,5 +1,6 @@
 import { Link } from "gatsby"
 import React from "react"
+import '../styles/styles.scss'
 
 import Header from '../components/header';
 import Seminaire from '../components/oculaire';
